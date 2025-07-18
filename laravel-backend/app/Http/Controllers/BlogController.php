3349ab6af5313e34c2d91533287488d7226b8fce
MD Touhedul Islam <110459773+ratul-prolific@users.noreply.github.com>
2025-07-18 15:39:19 +0600

    Blog api added

---
+++ laravel-backend/app/Http/Controllers/BlogController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use App\Http\Requests\StoreBlogRequest;
+use App\Http\Requests\UpdateBlogRequest;
+use App\Http\Resources\BlogResource;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return BlogResource::collection(Blog::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBlogRequest $request)
+    {
+        $blog = Blog::create($request->validated());
+        return new BlogResource($blog);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Blog $blog)
+    {
+        return new BlogResource($blog);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBlogRequest $request, Blog $blog)
+    {
+        $blog->update($request->validated());
+        return new BlogResource($blog);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Blog $blog)
+    {
+        $blog->delete();
+        return response()->noContent();
+    }
+}
